c4e8a63361f077324e0e4616326f7f7b1c908428
Travis Brown <travisb@travisbrown.ca>
2012-06-12 04:33:16 +0000

    A brief start. Mostly just parsing the datafile format.
    git-svn-id: svn+ssh://svnrepo/projects/nitpick/trunk@5004 561c3618-0fe6-0310-b5d6-fc65c73fee30

---
+++ nitpick.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+import os
+import fileinput
+import string
+
+
+# Root class of the VCS compatibility layer
+class VCS:
+	"""
+	Simple VCS which uses basic unix filesystem commands
+	"""
+	
+	def mkdir(self, path):
+		"""
+		Create a directory, creating any parent directories if necessary
+		"""
+		os.system("mkdir -p " + path)
+
+	def add_changes(self, path):
+		"""
+		Ensure that any changes made are registered with the VCS
+		"""
+		return
+
+	def commit(self, path_list):
+		"""
+		Ensure that all the registered changes are committed to the VCS repository.
+
+		This method receives a list of paths which must be committed.
+		"""
+		return
+
+class SVN(VCS):
+	def mkdir(self, path):
+		os.system("svn mkdir --parents " + path)
+
+	def add_changes(self, path):
+		os.system("svn add " + path);
+
+	def commit(self, path_list):
+		os.system("svn ci -m \"Nitpick commit\" " + " ".join(path_list))
+
+# A function which parses the given file into a dictionary with one entry for each piece of metadata
+# and a 'content' entry for the open ended content.
+def parse_file(path):
+	data = {}
+	for line in fileinput.input(path):
+		if 'content' not in data: # Process metadata
+			if line != '--\n':
+				fields = string.split(line, sep = ':', maxsplit = 1)
+				data[string.strip(fields[0])] = string.strip(fields[1])
+			else:
+				data['content'] = ""
+		else: # Add to the content
+			data['content'] += line
+
+	return data
+
+
